2e896584e7ca4c4745c868cb3ac05b6493d8e43c
Dogukan Kartal <dogukan_kartal98@hotmail.com>
2022-10-18 00:18:25 +0100

    first commit with xcode files

---
+++ TimesTables/TimesTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTablesApp.swift
+//  TimesTables
+//
+//  Created by Dogukan on 18/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct TimesTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
