67a295ea408716d911569c58fd377a41bc53be62
Dogukan Kartal <dogukan_kartal98@hotmail.com>
2022-10-18 01:04:11 +0100

    create form for user to enter their settings for the game

--- TimesTables/ContentView.swift
+++ TimesTables/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    let questionAmounts = [5, 10, 20]
+    
+    @State private var active = false
+    @State private var timesTable = 1
+    @State private var questionAmount = 5
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Times Tables")
+                .font(.largeTitle.bold())
+            
+            if active {
+                Text("hjkl")
+            } else {
+                Form {
+                    Section("Highest Multiple") {
+                        Stepper("Up to \(timesTable)", value: $timesTable, in: 1...12)
+                    }
+                    
+                    Section("Total Questions") {
+                        Picker("Questions", selection: $questionAmount) {
+                            ForEach(questionAmounts, id: \.self) {
+                                Text("\($0)")
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                    
+                    HStack {
+                        Spacer()
+                        
+                        Button("Submit") {
+                            active = true
+                        }
+                        
+                        Spacer()
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
